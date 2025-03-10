f8f786ed10517a6f8aaf20e82eaed2047585ef1b
ylalit0022 <ylalit0022@gmail.com>
2025-03-10 23:26:46 +0530

    admob ads server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use('/share', shareRoutes);
 app.use('/api/category-icons', categoryIconRoutes);
 app.use('/api/festivals', require('./routes/festivals'));
 app.use('/api/categoryIcons', require('./routes/categoryIcons'));
-apiRouter.use('/admob-ads', adMobRoutes);
+app.use('/admob-ads', adMobRoutes);
 
 
 
